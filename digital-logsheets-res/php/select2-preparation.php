a7c1cba1690235b701ff21ca8f38368a477de0a9
Evan Vassallo <evan.vassallo@me.com>
2016-07-04 21:25:36 -0400

    Fix path to select2 include

--- digital-logsheets-res/php/select2-preparation.php
+++ digital-logsheets-res/php/select2-preparation.php
@@ -1,5 +1,5 @@
 <?php
-include_once("../database/manageProgramEntries.php");
+include_once("database/manageProgramEntries.php");
 
 function getSelect2ProgramsList($db) {
     $programs = manageProgramEntries::getAllProgramsFromDatabase($db);
