8006d79859f07ebb8c8fafa55a08f4b4f215aa58
Evan Vassallo <evan.vassallo@me.com>
2017-06-14 15:32:39 -0400

    Correct indentation in index_login.php

--- public_html/index_login.php
+++ public_html/index_login.php
@@ -32,87 +32,87 @@ require_once("../digital-logsheets-res/php/objects/logsheetClasses.php");
 require_once("../digital-logsheets-res/php/DataPreparationForUI.php");
 include('../digital-logsheets-res/php/objects/User.php');
 include('session.php');
-       /*/cookie
-        if(!isset($_COOKIE['username'])) {
-            echo "<meta http-equiv='refresh' content='0;url=login_logsheet.php'>";
-            exit;
-        }
-        $username = $_COOKIE['username'];
-        echo 'Welcome '.$_COOKIE['username'].'</br>';
-        */
+/*/cookie
+ if(!isset($_COOKIE['username'])) {
+     echo "<meta http-equiv='refresh' content='0;url=login_logsheet.php'>";
+     exit;
+ }
+ $username = $_COOKIE['username'];
+ echo 'Welcome '.$_COOKIE['username'].'</br>';
+ */
 
 
-    //echo 'user id : '.$session_uid.'<br/>';
-    //echo 'program id : '.$session_program.'<br/>';
-    //echo 'program username : '.$session_programName.'<br/>';
+//echo 'user id : '.$session_uid.'<br/>';
+//echo 'program id : '.$session_program.'<br/>';
+//echo 'program username : '.$session_programName.'<br/>';
 
 
 $userClass = new User();
-    $userDetails = $userClass->userDetails($session_uid); // get user details
+$userDetails = $userClass->userDetails($session_uid); // get user details
 
-    if ($session_program == null) {
-        echo "<div class='row'>
+if ($session_program == null) {
+    echo "<div class='row'>
                 <div class='container-fluid'> 
                 <div class='col-sm-2'><h3>Admin</h3></div></div></div>";
-    } else {
-         // user information
-        echo "<div class='row'>
+} else {
+    // user information
+    echo "<div class='row'>
                 <div class='container-fluid'>";
-        echo "<h4 class='col-sm-7'>Show  name :" . $userDetails->name . "</h4> ";
-        echo "</div></div>";
-    }
-        //logout
-        echo "<div class='row'><div class='container-fluid'><h4 class='col-sm-7'><a href='logout.php'>Logout</a></h4></div></div>";
+    echo "<h4 class='col-sm-7'>Show  name :" . $userDetails->name . "</h4> ";
+    echo "</div></div>";
+}
+//logout
+echo "<div class='row'><div class='container-fluid'><h4 class='col-sm-7'><a href='logout.php'>Logout</a></h4></div></div>";
 
 
 // create object
-    $smarty = new Smarty;
+$smarty = new Smarty;
 
 //database interactions
-    try {
-        //open database connection
-        $db = connectToDatabase();
+try {
+    //open database connection
+    $db = connectToDatabase();
 
-        $archive = new Archive($db);
-        $episodesArchive = $archive->getArchive();
+    $archive = new Archive($db);
+    $episodesArchive = $archive->getArchive();
 
-        $episodes = array();
+    $episodes = array();
 
-        foreach ($episodesArchive as $episode) {
-            $playlist = array();
-            $segments = $episode->getPlaylist()->getSegments();
+    foreach ($episodesArchive as $episode) {
+        $playlist = array();
+        $segments = $episode->getPlaylist()->getSegments();
 
-            //create the playlist for each episode
-            if (is_array($segments) || is_object($segments)) {
-                foreach ($segments as $segment) {
-                    $playlist[$segment->getId()] = $segment->getObjectAsArray();
-                }
+        //create the playlist for each episode
+        if (is_array($segments) || is_object($segments)) {
+            foreach ($segments as $segment) {
+                $playlist[$segment->getId()] = $segment->getObjectAsArray();
             }
-
-            //create an array to store each episode's data
-            if ($session_program == NULL) {
-                $episodes[$episode->getId()] = $episode->getObjectAsArray();
-            } elseif ($episode->getProgram()->getId() == $session_program) {
-                $episodes[$episode->getId()] = $episode->getObjectAsArray();
-            }
-
         }
 
-        $programs = getSelect2ProgramsList($db); //program list
+        //create an array to store each episode's data
+        if ($session_program == NULL) {
+            $episodes[$episode->getId()] = $episode->getObjectAsArray();
+        } elseif ($episode->getProgram()->getId() == $session_program) {
+            $episodes[$episode->getId()] = $episode->getObjectAsArray();
+        }
 
-        //close database connection
-        $db = NULL;
-
-        $smarty->assign("episodes", $episodes);
-        $smarty->assign("programs", $programs);
-        //add program_id
-        $smarty->assign("program_id", $session_program);
-
-        // display it
-        echo $smarty->fetch('../digital-logsheets-res/templates/index_login.tpl');
-    } catch (PDOException $e) {
-        echo 'ERROR: ' . $e->getMessage();
     }
+
+    $programs = getSelect2ProgramsList($db); //program list
+
+    //close database connection
+    $db = NULL;
+
+    $smarty->assign("episodes", $episodes);
+    $smarty->assign("programs", $programs);
+    //add program_id
+    $smarty->assign("program_id", $session_program);
+
+    // display it
+    echo $smarty->fetch('../digital-logsheets-res/templates/index_login.tpl');
+} catch (PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
 
 ?>
 
