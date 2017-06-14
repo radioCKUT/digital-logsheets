32d8b3a78eadbf4d1f1c2f8b17ea6ac7189a7e5e
Evan Vassallo <evan.vassallo@me.com>
2017-06-14 16:06:05 -0400

    Add Donghee to copyright notice

--- public_html/save-segment.php
+++ public_html/save-segment.php
@@ -3,6 +3,7 @@
  * digital-logsheets: A web-based application for tracking the playback of audio segments on a community radio station.
  * Copyright (C) 2015  Mike Dean
  * Copyright (C) 2015-2017  Evan Vassallo
+ * Copyright (C) 2017 Donghee Baik
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -107,6 +108,7 @@ try {
     }
 
     $segmentValidator = new SegmentValidator($segment, $episode);
+
 
     if ($editSegment) {
         $segment->setId($segmentId);
--- public_html/templates_c/11a9d6931fb46bb0cbd6fe5afff818a8fcd5352c_0.file.add-segments.tpl.php
+++ public_html/templates_c/11a9d6931fb46bb0cbd6fe5afff818a8fcd5352c_0.file.add-segments.tpl.php
@@ -1,4 +1,23 @@
-<?php /* Smarty version 3.1.27, created on 2017-06-09 05:30:59
+<?php
+/**
+ * digital-logsheets: A web-based application for tracking the playback of audio segments on a community radio station.
+ * Copyright (C) 2015  Mike Dean
+ * Copyright (C) 2015-2017  Evan Vassallo
+ * Copyright (C) 2017 Donghee Baik
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */ /* Smarty version 3.1.27, created on 2017-06-09 05:30:59
          compiled from "/var/www/digital-logsheets-res/templates/add-segments.tpl" */ ?>
 <?php
 /*%%SmartyHeaderCode:1338208167593a32938609b0_55189214%%*/
